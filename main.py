860d22e16922d36e1a0c6298c61102d441a30033
Anast0sia <160764898+Anast0sia@users.noreply.github.com>
2024-09-28 17:29:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-dict = {}
-for i in range(0, 3):
-  data = input("Введите дату: ")
-  task = input("Введите задачу: ")
-  dict[data] = task
-for i in dict.keys():
-  print(f'{i} {dict[i]}')
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
+
+today = []
+tomorrow = []
+other = []
+tasks = []
+
+run = True
+
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(tasks)
+  elif command == "add":
+    data = input("Введите дату: ")
+    if data == "Сегодня":
+      today.append(data)
+    elif data == "Завтра":
+      tomorrow.append(data)
+    else:
+      other.append(data)
+    task = input("Введите название задачи: ")
+    tasks.append(task)
+    print("Задача добавлена")
+  elif command == "exit":
+    print("Спасибо за использование! ")
+    break
+  else:
+    print("Неизвестная команда! ")
+    break
+
+print("До свидания!")
